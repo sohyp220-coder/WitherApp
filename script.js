58299b30a935d2620c42d71e6494ed409e2c2c8a
sohyp <sohyp220@gmail.com>
2026-02-20 13:55:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 1. جلب موقع المستخدم تلقائياً
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showWeather);
 } else {
@@ -18,14 +17,14 @@ function showWeather(position) {
     document.body.appendChild(txt);
 
     const xhr = new XMLHttpRequest();
-    // الرابط كما هو مع استخدام المتغيرات الجديدة للموقع
+
     xhr.open('GET', `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,apparent_temperature_max,apparent_temperature_min,sunrise,temperature_2m_max,temperature_2m_min,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,rain_sum,snowfall_sum,precipitation_sum,showers_sum,precipitation_hours,precipitation_probability_max,wind_gusts_10m_max,wind_direction_10m_dominant,wind_speed_10m_max,shortwave_radiation_sum,et0_fao_evapotranspiration&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,dew_point_2m,rain,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_high,cloud_cover_mid,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_120m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_18cm,soil_temperature_54cm,soil_temperature_6cm,soil_moisture_0_to_1cm,soil_moisture_1_to_3cm,soil_moisture_3_to_9cm,soil_moisture_9_to_27cm,soil_moisture_27_to_81cm&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,snowfall,weather_code,cloud_cover,showers,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&past_days=1`, true);
 
     xhr.onload = function() {
         if (xhr.status === 200) {
             const data = JSON.parse(xhr.responseText);
             const temperatureNow = data.current.temperature_2m; 
-            const ln = data.current.is_day; // true للنهار، false لليل
+            const ln = data.current.is_day; 
             const wind = (data.current.wind_speed_10m / 3.6).toFixed(1);
             const humidity = data.hourly.relative_humidity_2m[0];
             const rainP = data.daily.precipitation_probability_max[0];
@@ -35,7 +34,7 @@ function showWeather(position) {
             txt.innerHTML = `
                 ${day}, ${day2} ${month}
                 <h2 style='font-size: 50px;'>${temperatureNow}°</h2>
-                <span> tomorrow: ${day2+1} ${maxfuture[1]}° </span>
+                <span> tomorrow ${day2+1} : ${maxfuture[1]}° </span>
                 <h6 style='margin-top: 30px;'>
                     Humidity: ${humidity}% <br>
                     Rain Probability 🌧️: ${rainP}% <br>
@@ -46,9 +45,9 @@ function showWeather(position) {
 
             const i = document.querySelector('i');
             
-            // ترتيب الشروط الصحيح:
+
             if (ln === 1 || ln === true) { 
-                // حالة النهار
+
                 if (temperatureNow >= 21) {
                     i.className = 'fa fa-sun';
                 } else if (temperatureNow >= 18) {
@@ -57,7 +56,7 @@ function showWeather(position) {
                     i.className = 'fa fa-cloud';
                 }
             } else { 
-                // حالة الليل
+
                 i.className = 'fa fa-cloud-moon';
             }
 
